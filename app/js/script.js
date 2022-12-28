004a8e13f0949231ff5b23685dd67e4e0da8c411
mohdxnizamv <nizam.active@gmail.com>
2022-12-28 14:21:20 +0800

    first commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,67 +1,67 @@
-const tabs = document.querySelectorAll('.tab')
-const panels = document.querySelectorAll('.panel')
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
-const logo = document.getElementById('logo')
+const tabs = document.querySelectorAll(".tab")
+const panels = document.querySelectorAll(".panel")
+const btn = document.getElementById("menu-btn")
+const menu = document.getElementById("menu")
+const logo = document.getElementById("logo")
 
 // Tabs menu event listener
-tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick))
 
 // Hamburger button listener
-btn.addEventListener('click', navToggle)
+btn.addEventListener("click", navToggle)
 
 function onTabClick(e) {
- // Deactivate all tabs
- tabs.forEach((tab) => {
-  tab.children[0].classList.remove(
-   'border-softRed',
-   'border-b-4',
-   'md:border-b-0'
-  )
- })
+  // Deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    )
+  })
 
- // Hide all panels
- panels.forEach((panel) => panel.classList.add('hidden'))
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add("hidden"))
 
- // Activate a new tab and panel based on the target
- e.target.classList.add('border-softRed', 'border-b-4')
- const classString = e.target.getAttribute('data-target')
- document
-  .getElementById('panels')
-  .getElementsByClassName(classString)[0]
-  .classList.remove('hidden')
+  // Activate a new tab and panel based on the target
+  e.target.classList.add("border-softRed", "border-b-4")
+  const classString = e.target.getAttribute("data-target")
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden")
 }
 
 function navToggle() {
- btn.classList.toggle('open')
- menu.classList.toggle('flex')
- menu.classList.toggle('hidden')
+  btn.classList.toggle("open")
+  menu.classList.toggle("flex")
+  menu.classList.toggle("hidden")
 
- if (menu.classList.contains('flex')) {
-  logo.setAttribute('src', './app/image/main-logo.png')
- } else {
-  logo.setAttribute('src', './app/image/main-logo.png')
- }
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./app/image/main-logo.png")
+  } else {
+    logo.setAttribute("src", "./app/image/main-logo.png")
+  }
 }
 
 // Get the button:
-let mybutton = document.getElementById('to-top')
+let mybutton = document.getElementById("to-top")
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
- scrollFunction()
+  scrollFunction()
 }
 
 function scrollFunction() {
- if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  mybutton.style.display = 'block'
- } else {
-  mybutton.style.display = 'none'
- }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block"
+  } else {
+    mybutton.style.display = "none"
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
- document.body.scrollTop = 0 // For Safari
- document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0 // For Safari
+  document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: ['**/*.html'],
- theme: {
-  screens: {
-   sm: '360px',
-   md: '760px',
-   lg: '1280px',
-   xl: '1440px',
-  },
-  extend: {
-   fontFamily: {
-    Alata: ['Alata', 'sans-serif'],
-    Outfit: ['Outfit', 'sans-serif'],
-    Josefin: ['Josefin Sans', 'sans-serif'],
-   },
-
-   colors: {
-    cyan: 'hsl(180, 66%, 49%)',
-    cyanLight: 'hsl(180, 66%, 69%)',
-    hijau: '#00ff00',
-    limegreen: '#32CD32',
-    darkGreen: '#3C4411',
-    darkOliveGreen: '#556b2f',
-    darkViolet: 'hsl(257, 27%, 26%)',
-    darkBlue: 'hsl(217, 28%, 15%)',
-    darkBlue1: 'hsl(218, 28%, 13%)',
-    darkBlue2: 'hsl(216, 53%, 9%)',
-    darkBlue3: 'hsl(219, 30%, 18%)',
-    red: 'hsl(0, 87%, 67%)',
-    grayishViolet: 'hsl(257, 7%, 63%)',
-    veryDarkBlue: 'hsl(255, 11%, 22%)',
-    veryDarkViolet: 'hsl(260, 8%, 14%) ',
-    'soft-blue': 'hsl(215, 51%, 70%)',
-    'dark-blue': {
-     'main-bg': 'hsl(217, 54%, 11%)',
-     'card-bg': 'hsl(216, 50%, 16%)',
-     line: 'hsl(215, 32%, 27%)',
+  content: ["**/*.html"],
+  theme: {
+    screens: {
+      sm: "360px",
+      md: "760px",
+      lg: "1280px",
+      xl: "1440px",
     },
-   },
+    extend: {
+      fontFamily: {
+        Alata: ["Alata", "sans-serif"],
+        Outfit: ["Outfit", "sans-serif"],
+        Josefin: ["Josefin Sans", "sans-serif"],
+      },
+
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        hijau: "#00ff00",
+        limegreen: "#32CD32",
+        darkGreen: "#3C4411",
+        darkOliveGreen: "#556b2f",
+        darkViolet: "hsl(257, 27%, 26%)",
+        darkBlue: "hsl(217, 28%, 15%)",
+        darkBlue1: "hsl(218, 28%, 13%)",
+        darkBlue2: "hsl(216, 53%, 9%)",
+        darkBlue3: "hsl(219, 30%, 18%)",
+        red: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
+        "soft-blue": "hsl(215, 51%, 70%)",
+        "dark-blue": {
+          "main-bg": "hsl(217, 54%, 11%)",
+          "card-bg": "hsl(216, 50%, 16%)",
+          line: "hsl(215, 32%, 27%)",
+        },
+      },
+    },
   },
- },
- plugins: [],
+  plugins: [],
 }
