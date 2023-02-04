cd98e114d2772295c92357d0c20a803ab75959db
Mohd Nizamuddin Mad Yusof <58153279+mohdxnizamv@users.noreply.github.com>
2023-02-04 23:58:26 +0800

    change navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  variants: {
+    extend: {
+      display: ["group-focus"],
+      opacity: ["group-focus"],
+      inset: ["group-focus"],
+    },
+  },
   content: ["**/*.html"],
   theme: {
     screens: {
@@ -13,30 +20,13 @@ module.exports = {
         Alata: ["Alata", "sans-serif"],
         Outfit: ["Outfit", "sans-serif"],
         Josefin: ["Josefin Sans", "sans-serif"],
+        Sans: ["Open Sans", "sans-serif"],
       },
 
       colors: {
-        cyan: "hsl(180, 66%, 49%)",
-        cyanLight: "hsl(180, 66%, 69%)",
-        hijau: "#00ff00",
-        limegreen: "#32CD32",
-        darkGreen: "#3C4411",
-        darkOliveGreen: "#556b2f",
-        darkViolet: "hsl(257, 27%, 26%)",
-        darkBlue: "hsl(217, 28%, 15%)",
-        darkBlue1: "hsl(218, 28%, 13%)",
-        darkBlue2: "hsl(216, 53%, 9%)",
-        darkBlue3: "hsl(219, 30%, 18%)",
-        red: "hsl(0, 87%, 67%)",
-        grayishViolet: "hsl(257, 7%, 63%)",
-        veryDarkBlue: "hsl(255, 11%, 22%)",
-        veryDarkViolet: "hsl(260, 8%, 14%) ",
-        "soft-blue": "hsl(215, 51%, 70%)",
-        "dark-blue": {
-          "main-bg": "hsl(217, 54%, 11%)",
-          "card-bg": "hsl(216, 50%, 16%)",
-          line: "hsl(215, 32%, 27%)",
-        },
+        "c.blue": "#484DFF",
+        bluedark: "#1A3066",
+        reddark: "#CC0000E6",
       },
     },
   },
